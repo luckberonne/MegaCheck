187890cf9cc962551731f0bb0a202193abd00f4e
Lucas Beronne <luckberonne@gmail.com>
2024-09-13 11:58:19 -0300

    Agregar archivos de proyecto.

---
+++ MegaCheck/Hubs/CheckBoxHub.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+public class CheckboxHub : Hub
+{
+    public async Task UpdateCheckboxState(int checkboxId, bool isChecked)
+    {
+        await Clients.All.SendAsync("ReceiveCheckboxUpdate", checkboxId, isChecked);
+    }
+}
